a9a4c5e8ac4fc1554f818067dc8278de781999e4
zhuanghs-demo <1565033737@qq.com>
2020-11-25 20:27:38 +0800

    zhuanghs:commit 201125

--- 01_interview/数据结构与算法篇/04_MyQueue/listQueue/listQueue.c
+++ 01_interview/数据结构与算法篇/04_MyQueue/listQueue/listQueue.c
@@ -5,5 +5,53 @@
  * @Author: Allen zhuang
  * @Date: 2020-11-24 20:21:13
  * @LastEditors: Allen Zhuang
- * @LastEditTime: 2020-11-24 20:21:14
+ * @LastEditTime: 2020-11-25 14:38:43
  */
+#include "listQueue.h"
+
+#include <stdlib.h>
+
+PtrToListQueue listQueue_create() {
+  PtrToListQueue q = (PtrToListQueue)malloc(sizeof(stListQueue));
+  if (!q) return NULL;
+  q->num = 0;
+  q->head = NULL;
+  q->tail = NULL;
+  return q;
+}
+
+void listQueue_destory(PtrToListQueue q) {
+  if (!q || listQueue_isEmpty(q)) return NULL;
+  int data = 0;
+  while (!listQueue_isEmpty(q)) listQueue_dequeue(q, &data);
+  free(q);
+  q = NULL;
+  return;
+}
+
+int listQueue_enqueue(PtrToListQueue q, int data) {
+  if (!q) return RET_ERROR;
+  PtrToListQueueNode node =
+      (PtrToListQueueNode)malloc(sizeof(stListQueue_node));
+  if (!node) return RET_ERROR;
+  if (!q->head) {
+    q->head = node;
+  } else {
+    q->tail->next = node;
+  }
+  q->tail = node;
+  ++q->num;
+  return RET_SUCC;
+}
+
+int listQueue_dequeue(PtrToListQueue q, int* data) {
+  if (!q || !data || listQueue_isEmpty(q)) return RET_ERROR;
+  PtrToListQueueNode tmpNode = q->head;
+  *data = tmpNode->data;
+  q->head = q->head->next;
+  --q->num;
+  if (!q->head) q->tail = NULL;
+  free(tmpNode);
+  tmpNode = NULL;
+  return RET_SUCC;
+}
--- 01_interview/数据结构与算法篇/04_MyQueue/listQueue/listQueue.h
+++ 01_interview/数据结构与算法篇/04_MyQueue/listQueue/listQueue.h
@@ -5,7 +5,7 @@
  * @Author: Allen zhuang
  * @Date: 2020-11-24 20:20:50
  * @LastEditors: Allen Zhuang
- * @LastEditTime: 2020-11-24 20:50:38
+ * @LastEditTime: 2020-11-25 09:40:02
  */
 #ifndef LIST_QUEUE_H
 #define LIST_QUEUE_H
@@ -19,10 +19,15 @@ typedef struct listQueue {
   int num;
   PtrToListQueueNode head;
   PtrToListQueueNode tail;
-} stListQueue, PtrToListQueue;
+} stListQueue, *PtrToListQueue;
 
 #define RET_SUCC (0)
 #define RET_ERROR (-1)
 #define listQueue_isEmpty(q) ((q->num) == 0)
+
+PtrToListQueue listQueue_create();
+void listQueue_destory(PtrToListQueue q);
+int listQueue_enqueue(PtrToListQueue q, int data);
+int listQueue_dequeue(PtrToListQueue q, int* data);
 
 #endif
